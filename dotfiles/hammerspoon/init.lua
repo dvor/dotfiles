43cf2a53a46212f51f3dd1fb22393512ade9c243
Dmytro Vorobiov <d@dvor.me>
2015-08-18 11:24:10 +0200

    hammerspoon stuff

--- dotfiles/hammerspoon/init.lua
+++ dotfiles/hammerspoon/init.lua
@@ -4,6 +4,8 @@
 hs.window.animationDuration = 0
 hs.window.setShadows(false)
 
+local hyper = {"cmd", "shift", "ctrl"}
+
 --------------------------------------------------------------------------------
 -- Confing reloading
 
@@ -24,7 +26,7 @@ hs.alert.show("Config reloaded")
 --------------------------------------------------------------------------------
 -- Locking screen
 
-hs.hotkey.bind({"cmd", "shift", "ctrl"}, "escape", function()
+hs.hotkey.bind(hyper, "escape", function()
     hs.caffeinate.startScreensaver()
     hs.alert.show("Locking screen")
 end)
@@ -32,7 +34,7 @@ end)
 --------------------------------------------------------------------------------
 -- Pasteboard
 
-hs.hotkey.bind({"cmd", "shift", "ctrl"}, "]", function()
+hs.hotkey.bind(hyper, "delete", function()
     hs.pasteboard.clearContents()
     hs.alert.show("Pasteboard cleared")
 end)
@@ -40,89 +42,208 @@ end)
 --------------------------------------------------------------------------------
 -- Window management
 
-hs.hotkey.bind({"cmd", "shift", "ctrl"}, "F", function()
-    local win = hs.window.focusedWindow()
-    local screen = win:screen()
-    local screenFrame = screen:frame()
+function equalFrames(first, second)
+    return (first.x == second.x) and (first.y == second.y) and (first.w == second.w) and (first.h == second.h)
+end
 
-    win:setFrame(screenFrame)
+function windowFullScreen()
+    return hs.screen.mainScreen():frame()
+end
 
+function windowLeft()
+    local frame = hs.screen.mainScreen():frame()
+    frame.w = frame.w * 2 / 3
+    return frame
+end
+
+function windowRight()
+    local frame = hs.screen.mainScreen():frame()
+    frame.x = frame.x + frame.w * 2 / 3
+    frame.w = frame.w / 3
+    return frame
+end
+
+function windowTop()
+    local frame = hs.screen.mainScreen():frame()
+    frame.h = frame.h * 2 / 3
+    return frame
+end
+
+function windowBottom()
+    local frame = hs.screen.mainScreen():frame()
+    frame.y = frame.y + frame.h * 2 / 3
+    frame.h = frame.h / 3
+    return frame
+end
+
+hs.hotkey.bind(hyper, "F", function()
+    hs.window.focusedWindow():setFrame(windowFullScreen())
     hs.alert.show("Fullscreen")
 end)
 
-hs.hotkey.bind({"cmd", "shift", "ctrl"}, "H", function()
-    local win = hs.window.focusedWindow()
-    local winFrame = win:frame()
-    local screen = win:screen()
-    local screenFrame = screen:frame()
+hs.hotkey.bind(hyper, "H", function()
+    local windowFrame = hs.window.focusedWindow():frame()
+    local frame = windowLeft()
+    local text = "Left"
 
-    winFrame.x = screenFrame.x
-    winFrame.y = screenFrame.y
-    winFrame.w = screenFrame.w * 2 / 3
-    winFrame.h = screenFrame.h
-    win:setFrame(winFrame)
+    if equalFrames(windowFrame, windowTop()) then
+        frame.h = windowTop().h
+        text = "Top/" .. text
+    elseif equalFrames(windowFrame, windowBottom()) then
+        frame.y = windowBottom().y
+        frame.h = windowBottom().h
+        text = "Bottom/" .. text
+    end
 
-    hs.alert.show("Left 2/3")
+    hs.window.focusedWindow():setFrame(frame)
+    hs.alert.show(text)
 end)
 
-k = hs.hotkey.modal.new({"cmd", "shift", "ctrl"}, "L")
+hs.hotkey.bind(hyper, "L", function()
+    local windowFrame = hs.window.focusedWindow():frame()
+    local frame = windowRight()
+    local text = "Right"
 
-function k:entered()
-    hs.alert.show("Right 1/3 -> {JKL}")
+    if equalFrames(windowFrame, windowTop()) then
+        frame.h = windowTop().h
+        text = "Top/" .. text
+    elseif equalFrames(windowFrame, windowBottom()) then
+        frame.y = windowBottom().y
+        frame.h = windowBottom().h
+        text = "Bottom/" .. text
+    end
+
+    hs.window.focusedWindow():setFrame(frame)
+    hs.alert.show(text)
+end)
+
+hs.hotkey.bind(hyper, "J", function()
+    local windowFrame = hs.window.focusedWindow():frame()
+    local frame = windowBottom()
+    local text = "Bottom"
+
+    if equalFrames(windowFrame, windowLeft()) then
+        frame.w = windowLeft().w
+        text = text .. "/Left"
+    elseif equalFrames(windowFrame, windowRight()) then
+        frame.x = windowRight().x
+        frame.w = windowRight().w
+        text = text .. "/Right"
+    end
+
+    hs.window.focusedWindow():setFrame(frame)
+    hs.alert.show(text)
+end)
+
+hs.hotkey.bind(hyper, "K", function()
+    local windowFrame = hs.window.focusedWindow():frame()
+    local frame = windowTop()
+    local text = "Top"
+
+    if equalFrames(windowFrame, windowLeft()) then
+        frame.w = windowLeft().w
+        text = text .. "/Left"
+    elseif equalFrames(windowFrame, windowRight()) then
+        frame.x = windowRight().x
+        frame.w = windowRight().w
+        text = text .. "/Right"
+    end
+
+    hs.window.focusedWindow():setFrame(frame)
+    hs.alert.show(text)
+end)
+
+--------------------------------------------------------------------------------
+-- Moving mouse
+
+local mouseMovingUp = false
+local mouseMovingBottom = false
+local mouseMovingRight = false
+local mouseMovingLeft = false
+local mouseIsDragging = false
+
+mouseMode = hs.hotkey.modal.new(hyper, "M")
+
+function mouseMode:entered()
+    hs.alert.show("Mouse mode")
+
+    local speedX = 0
+    local speedY = 0
+    local acceleration = 0.5
+
+    mouseTimer = hs.timer.doEvery(0.01, function()
+        if mouseMovingRight then
+            speedX = speedX + acceleration
+        elseif mouseMovingLeft then
+            speedX = speedX - acceleration
+        else
+            speedX = 0
+        end
+
+        if mouseMovingTop then
+            speedY = speedY - acceleration
+        elseif mouseMovingBottom then
+            speedY = speedY + acceleration
+        else
+            speedY = 0
+        end
+
+        local position = hs.mouse.getAbsolutePosition()
+        position.x = position.x + speedX
+        position.y = position.y + speedY
+        hs.mouse.setAbsolutePosition(position)
+
+        if mouseIsDragging then
+            hs.eventtap.event.newMouseEvent(hs.eventtap.event.types.leftmousedragged, position):post()
+        end
+    end)
 end
 
-k:bind({}, 'escape', function()
-    hs.alert.show("Canceled")
-    k:exit()
+function stopMouseMode()
+    hs.alert.show("Normal mode")
+    mouseTimer:stop()
+    mouseMode:exit()
+end
+
+mouseMode:bind({}, 'escape', stopMouseMode)
+mouseMode:bind(hyper, 'M', stopMouseMode)
+
+mouseMode:bind({}, 'H', function()
+    mouseMovingLeft = true
+end, function()
+    mouseMovingLeft = false
 end)
 
-k:bind({"cmd", "shift", "ctrl"}, 'J', function()
-    local win = hs.window.focusedWindow()
-    local winFrame = win:frame()
-    local screen = win:screen()
-    local screenFrame = screen:frame()
-
-    winFrame.x = screenFrame.x + screenFrame.w * 2 / 3
-    winFrame.y = screenFrame.y + screenFrame.h * 2 / 3
-    winFrame.w = screenFrame.w / 3
-    winFrame.h = screenFrame.h / 3
-    win:setFrame(winFrame)
-
-    hs.alert.show("Right 1/3, Bottom 1/3")
-
-    k:exit()
+mouseMode:bind({}, 'L', function()
+    mouseMovingRight = true
+end, function()
+    mouseMovingRight = false
 end)
 
-k:bind({"cmd", "shift", "ctrl"}, 'K', function()
-    local win = hs.window.focusedWindow()
-    local winFrame = win:frame()
-    local screen = win:screen()
-    local screenFrame = screen:frame()
-
-    winFrame.x = screenFrame.x + screenFrame.w * 2 / 3
-    winFrame.y = screenFrame.y
-    winFrame.w = screenFrame.w / 3
-    winFrame.h = screenFrame.h * 2 / 3
-    win:setFrame(winFrame)
-
-    hs.alert.show("Right 1/3, Top 2/3")
-
-    k:exit()
+mouseMode:bind({}, 'K', function()
+    mouseMovingTop = true
+end, function()
+    mouseMovingTop = false
 end)
 
-k:bind({"cmd", "shift", "ctrl"}, 'L', function()
-    local win = hs.window.focusedWindow()
-    local winFrame = win:frame()
-    local screen = win:screen()
-    local screenFrame = screen:frame()
+mouseMode:bind({}, 'J', function()
+    mouseMovingBottom = true
+end, function()
+    mouseMovingBottom = false
+end)
 
-    winFrame.x = screenFrame.x + screenFrame.w * 2 / 3
-    winFrame.y = screenFrame.y
-    winFrame.w = screenFrame.w / 3
-    winFrame.h = screenFrame.h
-    win:setFrame(winFrame)
+mouseMode:bind({}, 'F', function()
+    local position = hs.mouse.getAbsolutePosition()
+    hs.eventtap.event.newMouseEvent(hs.eventtap.event.types.leftmousedown, position):post()
 
-    hs.alert.show("Right 1/3")
+    mouseIsDragging = true
+end, function()
+    local position = hs.mouse.getAbsolutePosition()
+    hs.eventtap.event.newMouseEvent(hs.eventtap.event.types.leftmouseup, position):post()
 
-    k:exit()
+    mouseIsDragging = false
+end)
+
+mouseMode:bind({"shift"}, 'F', function()
+    hs.eventtap.rightClick(hs.mouse.getAbsolutePosition())
 end)
